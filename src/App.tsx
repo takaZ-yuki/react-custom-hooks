5588027709593cf208af315f5b1166d1c9349fac
yuki <gulgul.groove@gmail.com>
2022-03-25 00:40:44 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
+
+export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFeachUser = () => getUsers();
+
+  return (
+    <div className="App">
+      <button onClick={onClickFeachUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loadding...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
